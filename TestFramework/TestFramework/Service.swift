6a6814f69787da20d42360894a5f3dbd93d3e23c
Siva_Ns <sivansn10@gmail.com>
2018-09-28 11:35:35 +0530

    Finished our test framework

---
+++ TestFramework/TestFramework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  TestFramework
+//
+//  Created by CZ Ltd on 9/28/18.
+//  Copyright © 2018 CZ Ltd. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething()-> String {
+        
+        return "This is a test framewrok created by CZSM"
+    }
+}
